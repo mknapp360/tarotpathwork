691458cd2285c1081173764e92eb0a2c6d0e8b68
mknapp360 <mknapp360@gmail.com>
2025-10-21 23:55:03 +0100

    text

--- src/pages/ShemAngels.tsx
+++ src/pages/ShemAngels.tsx
@@ -32,21 +32,21 @@ export default function ShemAngels() {
 
       <section>
         <div className="mx-auto bg-tpblue ">
-          <h1 className="text-center py-8 text-4xl sm:text-5xl text-white font-display tracking-tight">
+          <h1 className="text-center py-8 text-4xl sm:text-5xl text-tpwhite font-display tracking-tight">
             Discover the Angels of Your Birth
           </h1>
-          <p className="mt-4 text-lg  mx-auto max-w-7xl text-white">
+          <p className="mt-4 text-lg  mx-auto max-w-7xl text-tpwhite">
             For centuries, mystics and sages have turned to the Shem HaMephorash—a sacred 72-fold name of God derived from three verses in the Book of Exodus—as a key to divine guidance and personal transformation. Each of these 72 names corresponds to an angelic intelligence: a spiritual force that oversees a specific aspect of human experience.
           </p>
-          <p className="mt-4 text-lg  mx-auto max-w-7xl text-white">
+          <p className="mt-4 text-lg  mx-auto max-w-7xl text-tpwhite">
             According to ancient Kabbalistic tradition, three of these angels are uniquely assigned to you at the moment of your birth. Together, they form a triad of spiritual guardians known as your Incarnation Angel, Morality Angel, and Soul Angel.
           </p>
-          <ul className="mt-4 ml-8 mr-8 text-lg text-tpblack">
+          <ul className="mt-4 text-lg  mx-auto max-w-7xl text-tpwhite">
             <li className="mb-2"><strong>Your Guardian Angel (Incarnation Angel) –</strong> governs your external life path. It influences your destiny, personality, and purpose. It reflects your strengths, challenges, and the divine mission you were born to fulfill in the world.</li>
             <li className="mb-2"><strong>Your Morality Angel (Heart Angel) –</strong> resides in the inner chamber of your heart. It speaks to your conscience, values, and emotional instincts. This angel guides your choices, your sense of right and wrong, and your ability to form deep relationships.</li>
             <li className="mb-2"><strong>Your Soul Angel (Intellect Angel) –</strong> reflects your connection to the Divine Mind. It influences your higher intellect, intuition, spiritual insight, and capacity for wisdom. This angel assists you in aligning your thoughts with your higher self.</li>
           </ul>
-          <p className="mt-4 text-lg  mx-auto max-w-7xl text-white">
+          <p className="mt-4 text-lg  mx-auto max-w-7xl text-tpwhite">
            I've had to code this calculator myself by turning the information found in ancient grimoires and texts, into modern usable code. The calculator uses your birth date and time to reveal the three angels associated with your incarnation. By learning their names and meanings, you can begin a journey of alignment with your divine blueprint—unlocking deeper purpose, clearer intuition, and inner peace.
           </p>
         </div>
