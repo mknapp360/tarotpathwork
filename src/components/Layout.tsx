91917d8b6fbea54416489b934cc2c01c41dbc17b
mknapp360 <mknapp360@gmail.com>
2025-10-21 20:39:00 +0100

    layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -34,7 +34,7 @@ export default function Layout() {
         ]}
       />
       <Navbar transparent={isHome} />
-      <main className={isHome ? '' : 'mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 py-10'}>
+      <main className={isHome ? '' : 'mx-auto'}>
         <Outlet />
       </main>
       <Footer />
